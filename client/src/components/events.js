9b6ce1ebaa992a08967bdb3f8ceabaf5a03f8a10
Jesper Vehn Munk <jemun@Jesper-Vehn-Munk.local>
2018-04-24 22:20:19 +0200

    Fetching events from Connector (hardcoded) in array.

--- client/src/components/events.js
+++ client/src/components/events.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import Connector from '../helpers/connector';
 
 export default class Events extends React.Component {
 
   render() {
-    var events = ['Mål', 'Frispark', 'Gult kort', 'Hjørne taget', 'Straffe begået', 'Straffe brændt', 'Skud på mål'];
+    //var events = ['Mål', 'Frispark', 'Gult kort', 'Hjørne taget', 'Straffe begået', 'Straffe brændt', 'Skud på mål'];
+    var events = Connector.getEvents();
+
     var eventRadios = [];
     var selectedOption = this.props.selectedOption;
     const onOptionChange = this.props.onOptionChange;
-    events.map(function(eventName, index) {
-      eventRadios.push(<div className="radio" key={index}>
+    events.map((event) => {
+      eventRadios.push(<div className="radio" key={event.Id}>
               <label>
-                <input className='bigradio' type="radio" value={eventName} key={index} checked={selectedOption === eventName} onChange={onOptionChange}/>
-                {eventName}
+                <input className='bigradio' type="radio" value={event.Name} key={event.Id} checked={selectedOption === event.Name} onChange={onOptionChange}/>
+                {event.Name}
               </label>
             </div>);
             return eventRadios;
--- client/src/helpers/connector.js
+++ client/src/helpers/connector.js
@@ -12,7 +12,22 @@ const getParticipants = () => {
   return result;
 }
 
+const getEvents = () => {
+  //Type 1 = Other 1
+  //Type 1 = Other 2
+  //Type 1 = Other 3
+  //Type 2 = Owner 1
+  //Type 2 = Owner 2
+  //Type 2 = Owner 3
+  var allEvents = [{Id:1, Name:'Goal', Type:'1', Small:10, Medium:20, Large:30, Walter:40},
+          {Id:2, Name:'Free kick committed', Type:'2', Small:40, Medium:30, Large:20, Walter:10},
+          {Id:3, Name:'Yellow card', Type:'2', Small:10, Medium:20, Large:30, Walter:40},
+          {Id:4, Name:'Shot on target', Type:'1', Small:10, Medium:20, Large:30, Walter:40}];
+  return allEvents;
+}
+
 export default {
   randomize,
-  getParticipants
+  getParticipants,
+  getEvents,
 }
